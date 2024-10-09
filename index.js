a8cf2dee651a25ae833766ca1c9611cf1b26993c
sarthak <thesarthakchauhan@gmail.com>
2024-10-09 16:27:21 +0530

    update

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const mongoose = require('mongoose')
 const port = 8083
 const db = require('./db');
+const User = require('./models/user.js');
 
 const routes = require('./routes/routes.js')
 const mastersRoutes = require('./routes/mastersRoutes.js')
@@ -29,6 +30,27 @@ app.delete('/event/delete/:id', (req, res) => {
   // Send a response back to acknowledge the delete request
   // res.status(200).json({ message: `Event ${eventId} deletion received` });
 });
+
+app.get('/search/:email', async (req, res) => {
+  const email = req.params.email;
+
+  try {
+    const user = await User.findOne({ email: email });
+
+    if (user) {
+      // User found, send user data as response
+      res.status(200).json(user);
+    } else {
+      // User not found
+      res.status(404).json({ message: 'User not found' });
+    }
+  } catch (error) {
+    console.error('Error searching for user:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
+ 
+
 
 
 
